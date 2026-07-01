21755f6fabcaafa73004d823f0c64ba7554a3246
OmkarDeware <omkardeware22@gmail.com>
2026-07-01 15:26:03 +0530

    Initial commit

---
+++ src/components/footer.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def  footer_home():
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center;">
+        <p style ="font-weight:bold; color:white; ">🎓 Learn Smarter, Teach Better</br>Crafted with ❤️ by Omkar Deware </p>
+        </div>
+                
+                
+                """,unsafe_allow_html = True)
+
+def  footer_dashboard():
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center;">
+        <p style ="font-weight:bold; color:black; ">🎓 Learn Smarter, Teach Better</br>Crafted with ❤️ by Omkar Deware </p>
+        </div>
+                
+                
+                """,unsafe_allow_html = True)
